00d924f62ec9f5c2354a7ec4483f310a4cbe82eb
rivasd <rivasdaniel1992@gmail.com>
2017-07-06 14:45:37 -0400

    removed unnecessary ids

--- src/schemas/session.js
+++ src/schemas/session.js
@@ -1,16 +1,18 @@
 
 const Trial = require("./trial");
 
-module.exports = function(app){
+module.exports = function (app) {
 
-    const mongooseClient = app.get('mongooseClient');
-    const TrialSchema = Trial(app);
+  const mongooseClient = app.get('mongooseClient');
+  const TrialSchema = Trial(app);
 
-    var Session = new mongooseClient.Schema({
-        browser: String,
-        data: [TrialSchema]
-    });
+  var Session = new mongooseClient.Schema({
+    browser: String,
+    data: [TrialSchema]
+  },
+    { _id: false }    //session inner objects have no need for id
+  );
 
-    return Session;
+  return Session;
 
 }
--- src/schemas/trial.js
+++ src/schemas/trial.js
@@ -1,15 +1,16 @@
 
-module.exports = function(app){
-    const mongooseClient = app.get('mongooseClient');
+module.exports = function (app) {
+  const mongooseClient = app.get('mongooseClient');
 
-    var  Trial = new mongooseClient.Schema({
-        trial_type:     {type: String, required: true},
-        trial_index:    {type: Number, required: true},
-        time_elapsed:   {type: Number, required: true},
-        internal_node_id:{type:String, required:true}
-    },{
-        strict: false
-    });
+  var Trial = new mongooseClient.Schema({
+    trial_type: { type: String, required: true },
+    trial_index: { type: Number, required: true },
+    time_elapsed: { type: Number, required: true },
+    internal_node_id: { type: String, required: true }
+  }, {
+    strict: false,
+    _id:false       //trials subdocument have no need for id
+  });
 
-    return Trial;
+  return Trial;
 }
